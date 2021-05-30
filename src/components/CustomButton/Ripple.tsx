2c79177e12a5dddd4f66d07616761e3c7d456535
gincheong <gincheong2@gmail.com>
2021-05-30 09:45:03 +0900

    feat: add CustomButton Components
    클릭 시 Ripple 효과 (multiple)
    selected: true일 시 색상 변화 있음
    
    close #1

---
+++ src/components/CustomButton/Ripple.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+import { isPropertySignature } from 'typescript';
+
+const StyledRipple = styled.div`
+  background: rgba(255, 255, 255, 0.5);
+
+  width: 10px;
+  height: 10px;
+  border-radius: 9999px;
+  position: absolute;
+
+  animation: 0.9s ease forwards ripples-effect;
+  @keyframes ripples-effect {
+    0% { transform: scale(1); opacity: 1; }
+    80% { transform: scale(50); }
+    100% { opacity: 0; }
+  }
+`;
+
+const Ripple = (props: Props) => {
+  return (
+    <StyledRipple
+      onAnimationEnd={props.onAnimationEnd}
+      style={{
+        left: props.coords.x,
+        top: props.coords.y
+      }}
+    />
+  );
+};
+
+interface Props {
+  onAnimationEnd: React.AnimationEventHandler;
+  coords: {
+    x: number,
+    y: number
+  };
+}
+
+export default Ripple;
