43d98a322c06c739653fc4afafbb6b3af006a84e
gincheong <gincheong2@gmail.com>
2021-06-02 19:37:35 +0900

    feat(Dashboard): 수정한 컴포넌트 적용

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 // Components
-import { Card, CardIcon, CardHeader, CardTitle, CardDescription, CardFooter } from '../components';
+import { CustomText } from '../components';
+import { Card, CardIcon, CardHeader, CardFooter, CardTitle } from '../components';
+import { Table, TableRow } from '../components';
 import { GridContainer, GridItem } from '../components';
+// DummyData
+import DummyTable from '../data/dummyTable';
 
 const Dashboard = () => {
 
   return (
+    <>
     <GridContainer>
       <GridItem>
         <Card>
           <CardHeader icon>
             <CardIcon src={"https://random.imagecdn.app/100/100"} />
-            <CardTitle>Used Space</CardTitle>
-            <CardDescription>
+            <CustomText gray>Used Space</CustomText>
+            <CustomText large>
               49/50 <small>GB</small>
-            </CardDescription>
+            </CustomText>
           </CardHeader>
           <CardFooter>Get more space</CardFooter>
         </Card>
@@ -23,8 +28,8 @@ const Dashboard = () => {
         <Card>
           <CardHeader icon>
             <CardIcon src={"https://random.imagecdn.app/101/101"} />
-            <CardTitle>Revenue</CardTitle>
-            <CardDescription>$34,245</CardDescription>
+            <CustomText gray>Revenue</CustomText>
+            <CustomText large>$34,245</CustomText>
           </CardHeader>
           <CardFooter>Last 24 Hours</CardFooter>
         </Card>
@@ -33,8 +38,8 @@ const Dashboard = () => {
         <Card>
           <CardHeader icon>
             <CardIcon src={"https://random.imagecdn.app/102/102"} />
-            <CardTitle>Fixed Issues</CardTitle>
-            <CardDescription>75</CardDescription>
+            <CustomText gray>Fixed Issues</CustomText>
+            <CustomText large>75</CustomText>
           </CardHeader>
           <CardFooter>Tracked from Github</CardFooter>
         </Card>
@@ -43,13 +48,70 @@ const Dashboard = () => {
         <Card>
           <CardHeader icon>
             <CardIcon src={"https://random.imagecdn.app/103/103"} />
-            <CardTitle>Followers</CardTitle>
-            <CardDescription>+245</CardDescription>
+            <CustomText gray>Followers</CustomText>
+            <CustomText large>+245</CustomText>
           </CardHeader>
           <CardFooter>Just Updated</CardFooter>
         </Card>
       </GridItem>
     </GridContainer>
+    <GridContainer>
+      <GridItem>
+        <Card>
+          <CardTitle>
+            1번 차트
+          </CardTitle>
+          <CustomText large>
+            <small>Daily Sales</small>
+          </CustomText>
+          <CustomText gray>55% Increase in today sales</CustomText>
+          <CardFooter>updated 4minutes ago</CardFooter>
+        </Card>
+      </GridItem>
+      <GridItem>
+        <Card>
+          <CardTitle>
+            2번 차트
+          </CardTitle>
+          <CustomText large>
+            <small>Email Subscriptions</small>  
+          </CustomText>
+          <CustomText gray>Last Campaign Performance</CustomText>
+          <CardFooter>campaign sent 2 days ago</CardFooter>
+        </Card>
+      </GridItem>
+      <GridItem>
+        <Card>
+          <CardTitle>
+            3번 차트
+          </CardTitle>
+          <CustomText large>
+            <small>Completed Tasks</small>
+          </CustomText>
+          <CustomText gray>Last Campaign Performance</CustomText>
+          <CardFooter>campaign sent 2 days ago</CardFooter>
+        </Card>
+      </GridItem>
+    </GridContainer>
+    <GridContainer>
+      <GridItem>
+        <Card>
+          <CardTitle>
+            <CustomText large>Employees Stats</CustomText>
+            <CustomText gray>New employees on 15th September, 2016</CustomText>
+          </CardTitle>
+          <Table head={['ID', 'Name', 'Salary', 'Country']}>
+            {DummyTable.map(each => <TableRow data={each} />)}
+          </Table>
+        </Card>
+      </GridItem>
+      <GridItem>
+        <Table>
+          
+        </Table>
+      </GridItem>
+    </GridContainer>
+    </>
   );
 };
 
