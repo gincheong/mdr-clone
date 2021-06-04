9702fed2f7bdc478b2dc7402ddfd8057c7857b6b
gincheong <gincheong2@gmail.com>
2021-06-04 18:05:50 +0900

    feat(CustomInput): Label props 추가
    props에 label을 전달하면 input이 활성화되거나, 컨텐츠가 입력된 경우에
    placeholder가 상단으로 움직이는 애니메이션 작동
    
    close #6

--- src/components/CustomInput/CustomInput.tsx
+++ src/components/CustomInput/CustomInput.tsx
@@ -35,17 +35,77 @@ const SInput = styled.input`
   }
 `;
 
+interface ILabel {
+  animation?: boolean;
+  restoreAnimation?: boolean;
+}
+const SLabel = styled.label<ILabel>`
+  position: absolute;
+  margin-top: 9px;
+  margin-left: 10px;
+  
+  font-size: 13px;
+  color: #848484;
+
+  animation: ${props => props.restoreAnimation ?
+    '0.1s ease forwards restoreLabel' :
+    (props.animation ?
+      '0.1s ease forwards moveLabel' : ''
+    )
+  };
+
+  @keyframes moveLabel {
+    100% { font-size: 11px; margin-top: -7px; }
+  }
+  @keyframes restoreLabel {
+    0% { font-size: 11px; margin-top: -7px; }
+    100% { font-size: 13px; margin-top: 9px; }
+  }
+`;
+
 const CustomInput = (props: Props) => {
-  const { type, placeholder, disabled } = props;
+  const { type, placeholder, disabled, label } = props;
 
   const [isFocused, setIsFocused] = React.useState(false);
+  const [value, setValue] = React.useState('');
+
+  const [animation, setAnimation] = React.useState(false);
+  const [showLabel, setShowLabel] = React.useState(true);
+  const [restoreAnimation, setRestoreAnimation] = React.useState(false);
+
+  React.useEffect(() => {
+    if (label) {
+      if (value !== '' || isFocused) {
+        setAnimation(true);
+      } else if (!isFocused) {
+        setRestoreAnimation(true);
+        setTimeout(() => {
+          setAnimation(false);
+          setRestoreAnimation(false);
+        }, 100);
+      }
+    } else {
+      if (value !== '') {
+        setShowLabel(false);
+      } else {
+        setShowLabel(true);
+      }
+    }
+  }, [value, label, isFocused]);
 
   return (
     <SContainer isFocused={isFocused}>
+      {showLabel &&
+        <SLabel htmlFor={placeholder} animation={animation} restoreAnimation={restoreAnimation}>
+          {placeholder}
+        </SLabel>
+      }
       <SInput
+        id={placeholder}
         type={type}
-        placeholder={placeholder ?? ''}
         disabled={disabled}
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
       />
@@ -57,6 +117,7 @@ interface Props {
   type: string;
   placeholder?: string;
   disabled?: boolean
+  label?: boolean;
 }
 
 export default CustomInput;
