ee1513c57e08ec1483b1c3d689a41e0a84572b27
gincheong <gincheong2@gmail.com>
2021-06-07 17:57:49 +0900

    fix(#12): 루트 컴포넌트의 라우팅에 <Switch> 추가
    Switch는 하위에 주어지는 path들 중에서, 현재 path와 일치하는 '제일 첫 번째' 컴포넌트를 렌더링함
    Switch를 사용하지 않으면 path에 해당되는 건 모두 렌더링한다
    루트에서 '/' 경로가 포함되면 '/main/dashboard'로 리렌더링 하게 했으니 새로고침하면 계속 dashboard로 이동했던 것
    
    close #12

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Redirect, Route } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 // Components
 import { Main } from './layouts';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Route path="/main" component={Main} />
-      <Redirect from="/" to="/main/dashboard" />
+      <Switch>
+        <Route path="/main" component={Main} />
+        <Redirect from="/" to="/main/dashboard" />
+      </Switch>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
