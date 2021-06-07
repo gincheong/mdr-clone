3c4bcdc9107a3bb9a313d7a6dc53288fa5fa1d61
gincheong <gincheong2@gmail.com>
2021-06-07 16:00:00 +0900

    feat(Notification): SnackbarButton 추가
    버튼 위치 등의 스타일은 아직 적용되지 않음

--- src/views/Notification.tsx
+++ src/views/Notification.tsx
@@ -1,9 +1,44 @@
+// Components
+import { GridContainer, GridItem } from '../components';
+import { Card, CardTitle } from '../components';
+import { CustomText } from '../components';
+import { SnackbarButton } from '../components';
+
 const Notification = () => {
 
   return (
-    <div>
-      Notification
-    </div>
+    <GridContainer>
+      <GridItem size={12}>
+        <Card>
+          <CardTitle>
+            <CustomText large>Notifications</CustomText>
+            <CustomText small>Handcrafted by our friends from Material UI and styled by Creative Tim. Please checkout the full documentation.</CustomText>
+          </CardTitle>
+          <GridContainer>
+              <SnackbarButton position="top-left" buttonText="top-left">
+                Welcome to MATERIAL DASHBOARD React - a beautiful freebie for every web developer.
+              </SnackbarButton>
+              <SnackbarButton position="top-center" buttonText="Top-Center">
+                Welcome to MATERIAL DASHBOARD React - a beautiful freebie for every web developer.
+              </SnackbarButton>
+              <SnackbarButton position="top-right" buttonText="top-right">
+                Welcome to MATERIAL DASHBOARD React - a beautiful freebie for every web developer.
+              </SnackbarButton>
+          </GridContainer>
+          <GridContainer>
+              <SnackbarButton position="bottom-left" buttonText="bottom-left">
+                Welcome to MATERIAL DASHBOARD React - a beautiful freebie for every web developer.
+              </SnackbarButton>
+              <SnackbarButton position="bottom-center" buttonText="bottom-center">
+                Welcome to MATERIAL DASHBOARD React - a beautiful freebie for every web developer.
+              </SnackbarButton>
+              <SnackbarButton position="bottom-right" buttonText="bottom-right">
+                Welcome to MATERIAL DASHBOARD React - a beautiful freebie for every web developer.
+              </SnackbarButton>
+          </GridContainer>
+        </Card>
+      </GridItem>
+    </GridContainer>
   );
 };
 
